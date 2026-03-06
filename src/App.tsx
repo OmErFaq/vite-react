4ab39011be8948fec46a02a8032b730fd6f15f05
OmErFaq <68440945+OmErFaq@users.noreply.github.com>
2026-03-06 21:12:24 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { useState, useEffect } from "react";
-import { initializeApp } from "firebase/app";
 import {
   getFirestore, doc, getDoc, setDoc, onSnapshot,
-  collection, addDoc, getDocs, DocumentData
+  collection, getDocs, DocumentData
 } from "firebase/firestore";
 
 const firebaseConfig = {
